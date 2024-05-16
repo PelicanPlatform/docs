2e83e9852d6aff2b70e589a39761c6bc48cb4bb8
Haoming Meng <hmeng29@wisc.edu>
2024-05-16 10:21:54 -0500

    Fix jumpiness in the config page

--- utils/darkLightContainer.tsx
+++ utils/darkLightContainer.tsx
@@ -1,39 +1,22 @@
-import React, { use, useEffect, useState } from "react";
-import { PaletteMode, ThemeProvider, createTheme } from '@mui/material';
-import { useTheme } from 'next-themes';
-import CssBaseline from '@mui/material/CssBaseline';
+import React from "react";
+import { PaletteMode } from '@mui/material'
+import {  ThemeProvider, createTheme } from '@mui/material/styles';
+import { ThemeProvider as NextThemeProvider, useTheme } from 'next-themes';
 
 export const DarkLightContainer = ({ children }) => {
     const { resolvedTheme } = useTheme();
-    const [theme, setTheme] = useState('dark');
-    useEffect(() => {
-        let mounted = true;
-        if (resolvedTheme === 'system') {
-            if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
-                setTheme('dark');
-            }
-            else {
-                setTheme('light');
-            }
-        } else {
-            setTheme(resolvedTheme);
-        }
-
-        return () => {
-            mounted = false;
-        };
-    }, [resolvedTheme]);
 
     const muiTheme = createTheme({
         palette: {
-            mode: theme as PaletteMode,
+            mode: resolvedTheme as PaletteMode,
         }
     });
 
     return (
-        <ThemeProvider theme={muiTheme}>
-            <CssBaseline />
-            {children}
-        </ThemeProvider>
+        <NextThemeProvider>
+            <ThemeProvider theme={muiTheme}>
+                {children}
+            </ThemeProvider>
+        </NextThemeProvider>
     )
 }
