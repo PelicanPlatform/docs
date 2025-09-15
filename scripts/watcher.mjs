c46db67c61acc9275ada7ec4dc54ddf231ab587d
Cannon Lock <clock@wisc.edu>
2025-09-15 15:25:35 -0500

    Fixup prebuild

--- scripts/watcher.mjs
+++ scripts/watcher.mjs
@@ -1,30 +1,10 @@
 import path from "path";
 import fs from "fs";
 
+import { setup } from "./setup.mjs";
+
 const app_dir = path.resolve(process.env.PELICAN_PATH) + '/docs/app'
 const public_dir = path.resolve(process.env.PELICAN_PATH) + '/docs/public'
-
-const setup = () => {
-	// Copy over the dir contents from the pelican repo to the app directory
-	// Set up the pages symlink to point at pelican repo identified by PELICAN_PATH
-	fs.rmSync('./app', { recursive: true, force: true });
-	fs.cpSync(
-			app_dir,
-			'./app',
-			{recursive: true},
-			(err) => console.log(err)
-	);
-
-	// Copy over the dir contents from the pelican repo to the public directory
-	// Set up the public symlink to point at pelican repo identified by PELICAN_PATH
-	fs.rmSync('./public/pelican', { recursive: true, force: true });
-	fs.cpSync(
-			public_dir,
-			'./public/pelican',
-			{recursive: true},
-			(err) => console.log(err)
-	);
-}
 
 const updateFiles = (file) => {
 	if (file.startsWith('app/')) {
